8be7e52ac517660cd1c9bdd443ecd34b2055e36f
dddodong <cjfdnjs77@gmail.com>
2025-07-15 18:28:15 +0900

    Tailwind 설정 파일 수동 추가 마지막

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // 
   ],
   theme: {
     extend: {},
